1a269b26a8733e6ef93e5e4d97c110f05d2928cc
Luís Puhl <luis.puhl@gmail.com>
2024-03-14 12:09:16 -0300

    happy coding, working leak

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,31 +3,38 @@ use std::time::{Duration, SystemTime};
 #[derive(Debug)]
 struct Bucket {
     pub instant: SystemTime,
-    pub counter: usize,
-    pub limit: usize,
-    pub rate: usize,
-    pub interval: usize,
+    pub counter: u128,
+    pub limit: u128,
+    pub rate: u128,
+    pub interval: Duration,
 }
 
 impl Bucket {
-    fn new() -> Bucket {
-        return Bucket {
-            instant: SystemTime::now(),
-            counter: 0,
-            limit: 100,
-            rate: 100,
-            interval: 60,
-        };
-    }
-
-    fn ingest(&mut self) -> &mut Self {
+    fn ingest(&mut self, new_instant: SystemTime) -> &mut Self {
         self.counter += 1;
-        self.instant = SystemTime::now();
+        self.instant = new_instant;
         self
     }
 
     fn is_limited(&self) -> bool {
         return self.counter > self.limit;
+    }
+
+    fn leak(&mut self, new_instant: SystemTime) -> &mut Self {
+        let elapsed = new_instant
+            .duration_since(self.instant)
+            .unwrap_or_else(|_error| Duration::ZERO);
+        if elapsed < self.interval {
+            return self;
+        }
+        let intervals = elapsed.as_micros() / self.interval.as_micros();
+        if intervals < 1 {
+            return self;
+        }
+        let leak = intervals * self.rate;
+        self.counter -= self.counter.min(leak);
+        self.instant = new_instant;
+        self
     }
 }
 
@@ -49,12 +56,26 @@ impl RateLimit for Bucket {
 
 fn main() {
     println!("Hello, world!");
-    let mut bucket: Bucket = Bucket::new();
+    let mut bucket: Bucket = Bucket {
+        instant: SystemTime::now(),
+        counter: 0,
+        limit: 100,
+        rate: 100,
+        interval: Duration::from_secs(10),
+    };
+
     println!("{:#?}, limited: {:#?}", bucket, bucket.is_limited());
 
     bucket.counter += 1;
     println!("{:#?}, limited: {:#?}", bucket, bucket.is_limited());
 
-    bucket.ingest();
+    bucket.ingest(SystemTime::now());
+    println!("{:#?}, limited: {:#?}", bucket, bucket.is_limited());
+
+    bucket.leak(
+        SystemTime::now()
+            .checked_add(Duration::from_secs(10))
+            .expect("Can add 10s to now()"),
+    );
     println!("{:#?}, limited: {:#?}", bucket, bucket.is_limited());
 }
