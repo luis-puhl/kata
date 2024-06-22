d9ef10651d24bac448a539b6f2438e40a667ef8f
Luís Puhl <luis.puhl@gmail.com>
2024-06-22 07:55:56 -0300

    add 404

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,22 +1,43 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
-use std::fs;
+
+fn get() -> String {
+    let contents = fs::read_to_string("index.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    response
+}
+
+fn http404() -> String {
+    let contents = fs::read_to_string("404.html").unwrap();
+    let response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n{}", contents);
+    response
+}
 
 fn handle_connection(mut stream: std::net::TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let mut req_lines = request.lines();
+    let path = req_lines.next().unwrap();
+    println!("Request: {}", path);
     
-    let contents = fs::read_to_string("index.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
+    let get_prefix = b"GET / HTTP/1.1\r\n";
+    let response = if buffer.starts_with(get_prefix) {
+        get()
+    } else {
+        http404()
+    };
+    println!("Response: {}", response.lines().next().unwrap());
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
 fn main() {
     println!("Hello, world!");
+    let addr = "127.0.0.1:8080";
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Listening on http://{}", addr);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established!");
